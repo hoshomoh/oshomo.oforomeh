551265418ccb24424715a425afbf33428adaf28d
Oshomo Oforomeh <oshomo.oforomeh2010@gmail.com>
2025-12-30 13:57:22 +0100

    chore: Upgrade to next16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    localPatterns: [
+      {
+        pathname: '/images/**',
+      },
+      {
+        pathname: '/posts/**',
+      },
+    ],
+  },
   async redirects() {
     return [
       {
